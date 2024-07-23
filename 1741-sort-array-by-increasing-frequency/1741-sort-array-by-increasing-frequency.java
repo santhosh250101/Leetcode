8aa8ae45620a4652d499ce49ce1fcf29453ef746
santhosh250101 <52774825+santhosh250101@users.noreply.github.com>
2024-07-23 22:11:27 +0530

    Time: 7 ms (55.67%), Space: 44.3 MB (72.73%) - LeetHub

--- 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -1,33 +1,60 @@
 class Solution {
     public int[] frequencySort(int[] nums) {
-        
-        HashMap<Integer,Integer> fMap = new HashMap<>();
 
-        for(int num: nums){
-            fMap.put(num,fMap.getOrDefault(num,0)+1);
+        Integer[] numbers = new Integer[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            numbers[i] = nums[i];
         }
-        int n = nums.length;
-        int[] res = new int[n];
+        Map<Integer, Integer> freq = new HashMap<>();
+        for (int num : nums) {
+            freq.put(num, freq.getOrDefault(num, 0) + 1);
+        }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-            if(fMap.get(a) == fMap.get(b)){
-                return b-a;
+
+        Arrays.sort(numbers,(a,b)->{
+            if(freq.get(a) == freq.get(b)){
+                return Integer.compare(b,a);
             }
-            return fMap.get(a)-fMap.get(b);
+            return freq.get(a)- freq.get(b);
         });
-
-
-        pq.addAll(fMap.keySet());
-        int i=0;
-        while(!pq.isEmpty()){
-
-            int top = pq.poll();
-            int freq = fMap.get(top);
-            while(freq>0){
-                res[i++]= top;
-                freq--;
-            }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = numbers[i];
         }
-        return res;
+        return nums;
+
+
+        
+        // HashMap<Integer,Integer> fMap = new HashMap<>();
+
+        // for(int num: nums){
+        //     fMap.put(num,fMap.getOrDefault(num,0)+1);
+        // }
+        // int n = nums.length;
+        // int[] res = new int[n];
+
+        // PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+        //     if(fMap.get(a) == fMap.get(b)){
+        //         return b-a;
+        //     }
+        //     return fMap.get(a)-fMap.get(b);
+        // });
+
+
+        // pq.addAll(fMap.keySet());
+        // int i=0;
+        // while(!pq.isEmpty()){
+
+        //     int top = pq.poll();
+        //     int freq = fMap.get(top);
+        //     while(freq>0){
+        //         res[i++]= top;
+        //         freq--;
+        //     }
+        // }
+        // return res;
+
+
+
     }
 }
