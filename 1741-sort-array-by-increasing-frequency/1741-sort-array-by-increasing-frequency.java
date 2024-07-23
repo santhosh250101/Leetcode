5e546fe57af814deffaf80ead635e0f39d1e00c0
santhosh250101 <52774825+santhosh250101@users.noreply.github.com>
2024-07-23 22:06:17 +0530

    Time: 11 ms (24.16%), Space: 44.3 MB (72.73%) - LeetHub

---
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+        
+        TreeMap<Integer,Integer> fMap = new TreeMap<>(Collections.reverseOrder());
+
+        for(int num: nums){
+            fMap.put(num,fMap.getOrDefault(num,0)+1);
+        }
+        int n = nums.length;
+        int[] res = new int[n];
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            if(fMap.get(a) == fMap.get(b)){
+                return b-a;
+            }
+            return fMap.get(a)-fMap.get(b);
+        });
+
+
+        pq.addAll(fMap.keySet());
+        int i=0;
+        while(!pq.isEmpty()){
+
+            int top = pq.poll();
+            int freq = fMap.get(top);
+            while(freq>0){
+                res[i++]= top;
+                freq--;
+            }
+        }
+        return res;
+    }
+}
