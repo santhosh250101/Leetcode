d1111e650bdf8bbc850dd57136011907de405d05
santhosh250101 <52774825+santhosh250101@users.noreply.github.com>
2025-06-30 01:14:22 +0530

    Time: 2 ms (25.56%), Space: 42.4 MB (80.55%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,40 +1,54 @@
 class Solution {
     public int myAtoi(String s) {
-        
         s= s.trim();
+        if(s.length()==0) return 0;
+        boolean isNegative = false;
+        long ans = 0;
 
-        long ans=0;
-        int sign = 1;
         for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
             if(i==0){
-                if(s.charAt(i)== '-'){
-                    sign= -1;
-                    continue;
+                if(s.charAt(i) == '-'){
+                    isNegative = true;
                 }
-                else if(s.charAt(i)== '+'){
-                    sign = 1;
+                else if(s.charAt(i) == '+'){
                     continue;
-                }
-            }
-            int digit = s.charAt(i) -'0';
-            if(digit>=0 && digit<=9){
-                ans = (ans*10)+ digit;
-                if(sign == -1){
-                    if(-ans <Integer.MIN_VALUE){
-                        return Integer.MIN_VALUE;
-                    }
                 }
                 else{
-                    if(ans>Integer.MAX_VALUE){
-                        return Integer.MAX_VALUE;
+                    if(ch>='0' && ch<='9'){
+                        int digit = s.charAt(i) -'0';
+                        ans = (ans *10)+ digit;
+                    }
+                    else{
+                        if(isNegative) return -(int)ans;
+                        return (int)ans;
                     }
                 }
             }
             else{
-                break;
+                if(ch>='0' && ch<='9'){
+                    int digit = s.charAt(i) -'0';
+                    ans = (ans *10)+ digit;
+
+                    if(isNegative){
+                        if(-ans< Integer.MIN_VALUE){
+                            return Integer.MIN_VALUE;
+                        }
+                    }
+                    else{
+                        if(ans>Integer.MAX_VALUE){
+                            return Integer.MAX_VALUE;
+                        }
+                    }
+                }
+                else{
+                    if(isNegative) return -(int)ans;
+                    return (int)ans;
+                }
             }
         }
-        ans = ans*sign;
-        return (int) ans;
+        if(isNegative) return -(int)ans;
+        return (int)ans;
+
     }
 }
