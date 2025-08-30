f6f99260f9ae3cae415b7d6f8fc619331bc32b7e
santhosh250101 <52774825+santhosh250101@users.noreply.github.com>
2025-08-30 13:30:46 +0530

    Time: 0 ms (100%), Space: 43.2 MB (38.57%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,34 +1,37 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        // find the flloor off te array 
-        // such that arr[i]<= target
-        //i both sae retun i, else return i+1
+        
+        int low=0;
+        int high = nums.length-1;
+        int ans =-1;
+        while(low<=high){
+            int mid = low+ (high-low)/2;
 
-        int n= nums.length;
-        int low = 0;
-        int high = n-1;
-        int ans = n-1;
-
-        while(low<= high){
-
-            int mid = (low+high)/2;
-
-            if(nums[mid]<= target){
+            if(nums[mid]==target){
                 ans = mid;
-                low= mid+1;
+                return ans;
             }
-            else
-            {
+            else if(target<nums[mid]){
+                ans = mid;
                 high = mid-1;
             }
+            else{
+                low= mid+1;
+            }
         }
 
-        int floorIndex=  nums[ans]<= target?ans : -1;
-
-        if(floorIndex ==-1){
+        if(target<nums[0]){
             return 0;
         }
+        if(target> nums[nums.length-1]){
+            return nums.length;
+        }
+        return ans;
+        /**
+        
+        1 2 3
+         */
 
-        return nums[floorIndex] == target ? floorIndex : floorIndex+1;
+
     }
 }
