1d9a1a6865711c0f0ea09b47f843c95c8ef2bb0a
santhosh250101 <52774825+santhosh250101@users.noreply.github.com>
2025-09-06 16:59:15 +0530

    Time: 0 ms (100%), Space: 41.5 MB (39.13%) - LeetHub

--- 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/1878-check-if-array-is-sorted-and-rotated.java
@@ -1,20 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        
+        // there wiill be peak
         int n = nums.length;
-        int countDrops =0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]<=nums[(i+1)%n]){
-                continue;
-            }
-            else
-            {
-                countDrops++;
-            }
+        int count=0;
+        for(int i=0;i<n;i++){
 
-            if(countDrops>1) return false;
+            if(nums[i]> (nums[(i+1)%n])){
+                count++;
+            }
         }
+        if(count>1) return false;
         return true;
     }
 }
